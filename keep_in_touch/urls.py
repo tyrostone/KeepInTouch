ce081ef9607aef47c66399b48f72f0996a57da6c
Laura Stone <lstone@veracode.com>
2017-05-05 14:43:05 -0400

    Add requirements file, URL regex modifications

--- keep_in_touch/urls.py
+++ keep_in_touch/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'$', include('contact_selector.urls')),
+    url(r'contact/', include('contact_selector.urls')),
 ]
